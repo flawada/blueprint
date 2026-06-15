2dc5eb23ad5a58edf54af14bd4ca531a87a70446
flawada <e.sauermann@icloud.com>
2026-06-15 15:22:24 +0200

    add dependency

--- blueprints/mangowm-fedora/setup.sh
+++ blueprints/mangowm-fedora/setup.sh
@@ -2,7 +2,7 @@ sudo dnf update -y
 
 sudo dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release -y
 
-sudo dnf in mangowm foot thunar waybar mako wlsunset swaybg wl-clipboard gtklock playerctl rofi wlogout blueman-manager pavucontrol nm-connection-editor xorg-x11-server-Xwayland xfce-polkit cliphist -y
+sudo dnf in mangowm foot thunar waybar mako wlsunset swaybg wl-clipboard gtklock playerctl rofi wlogout blueman-manager pavucontrol nm-connection-editor xorg-x11-server-Xwayland xfce-polkit cliphist tar -y
 
 sudo dnf in nwg-look gedit engrampa firefox xdg-user-dirs -y
 xdg-user-dirs-update
