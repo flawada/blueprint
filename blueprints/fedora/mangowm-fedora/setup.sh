55627f22c665d425da0eeda0c1bef9061a996f30
flawada <e.sauermann@icloud.com>
2026-06-16 14:51:13 +0200

    fix setup

--- blueprints/fedora/mangowm-fedora/setup.sh
+++ blueprints/fedora/mangowm-fedora/setup.sh
@@ -2,16 +2,15 @@ sudo dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/t
 
 sudo dnf in mangowm foot thunar waybar mako wlsunset swaybg wl-clipboard cliphist gtklock playerctl rofi wlogout blueman-manager pavucontrol nm-connection-editor xorg-x11-server-Xwayland xfce-polkit tar -y
 
-sudo dnf in nwg-look xdg-user-dirs -y
+sudo dnf in nwg-look xdg-user-dirs fish -y
 xdg-user-dirs-update
 
 #gedit engrampa firefox
 
-cp -r /tmp/blueprint/blueprints/fedora/mangowm-fedora/config/* ~/.config/
+cp -r /tmp/config/blueprints/fedora/mangowm-fedora/config/* ~/.config/
 curl -Lso ~/.config/mango/wallpaper.png https://w.wallhaven.cc/full/xe/wallhaven-xe7ylv.png
 
 curl -sS https://starship.rs/install.sh | sh -s -- -y
-sudo dnf install fish
 chsh -s /usr/bin/fish
 echo "set -g fish_greeting" >> ~/.config/fish/config.fish
 echo "starship init fish | source" >> ~/.config/fish/config.fish
