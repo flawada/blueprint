25078179aa7d2d47fd4509198cef4bdd8fa36b95
flawada <e.sauermann@icloud.com>
2026-06-22 20:51:57 +0200

    fix

--- blueprints/fedora/mango/files/setup.sh
+++ blueprints/fedora/mango/files/setup.sh
@@ -2,7 +2,7 @@ sudo dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/t
 sudo dnf copr enable -y leloubil/wl-clip-persist
 sudo dnf copr enable -y sneexy/zen-browser
 
-sudo dnf in -y mangowm ghostty thunar waybar mako wlsunset swaybg wl-clip-persist cliphist gtklock playerctl rofi wlogout blueman-manager pavucontrol nm-connection-editor xdg-desktop-portal xdg-desktop-portal-wlr xfce-polkit gedit nwg-look xdg-user-dirs zsh eza zen-browser git
+sudo dnf in -y mangowm ghostty thunar waybar mako wlsunset swaybg wl-clip-persist cliphist gtklock playerctl rofi wlogout blueman-manager pavucontrol nm-connection-editor xdg-desktop-portal xdg-desktop-portal-wlr xorg-x11-server-Xwayland xfce-polkit gedit nwg-look xdg-user-dirs zsh eza zen-browser git
 
 xdg-user-dirs-update
 
@@ -12,10 +12,10 @@ cp -r /tmp/files/home/. ~
 curl -Lso ~/.config/mango/wallpaper.png https://w.wallhaven.cc/full/xe/wallhaven-xe7ylv.png
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-git clone git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
-chsh -s $(which zsh)
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
+sudo chsh -s $(which zsh)
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 
 printf '[Service]\nExecStart=\nExecStart=-/usr/sbin/agetty --autologin %s --noclear %%I $TERM\n' "$USER" | sudo systemctl edit getty@tty1 --stdin
 
-mango
+reboot
