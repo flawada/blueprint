ade5bfb9e1db294a43bd0c725d3ae4e59c273d7c
flawa <e.sauermann@icloud.com>
2026-06-26 23:30:52 +0200

    Enable zen-browser installation in setup script

--- blueprints/fedora/mango/files/setup.sh
+++ blueprints/fedora/mango/files/setup.sh
@@ -1,10 +1,10 @@
 sudo dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release -y
 sudo dnf copr enable -y leloubil/wl-clip-persist
-#sudo dnf copr enable -y sneexy/zen-browser
+sudo dnf copr enable -y sneexy/zen-browser
 
-sudo dnf in -y mangowm ghostty thunar waybar mako wlsunset swaybg wl-clip-persist cliphist gtklock playerctl rofi wlogout blueman-manager pavucontrol nm-connection-editor xdg-desktop-portal xdg-desktop-portal-wlr xorg-x11-server-Xwayland xfce-polkit gedit nwg-look xdg-user-dirs zsh eza git
+sudo dnf in -y mangowm ghostty thunar zen-browser waybar mako wlsunset swaybg wl-clip-persist cliphist gtklock playerctl rofi wlogout blueman-manager pavucontrol nm-connection-editor xdg-desktop-portal xdg-desktop-portal-wlr xorg-x11-server-Xwayland xfce-polkit gedit nwg-look xdg-user-dirs zsh eza git
 
-# engrampa zen-browser
+# engrampa
 
 xdg-user-dirs-update
 
