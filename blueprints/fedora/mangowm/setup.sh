aa6a10094a5c458b0d2480889d226dd549f6f84b
flawa <e.sauermann@icloud.com>
2026-07-10 18:26:12 +0200

    Fix autologin setup in setup.sh script

--- blueprints/fedora/mangowm/setup.sh
+++ blueprints/fedora/mangowm/setup.sh
@@ -133,7 +133,7 @@ fi
 if ! grep -q -- "--autologin $USER" /etc/systemd/system/getty@tty1.service.d/override.conf &> /dev/null; then
   printf "\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n"
   printf "%bEnabling autologin..%b\n" "$BLUE" "$NC"
-  printf '[Service]\nExecStart=-/usr/sbin/agetty --autologin %s --noclear %%I $TERM\n' "$USER" | sudo systemctl edit getty@tty1 --stdin
+  printf '[Service]\nExecStart=\nExecStart=-/usr/sbin/agetty --autologin %s --noclear %%I $TERM\n' "$USER" | sudo systemctl edit getty@tty1 --stdin
   printf "%bEnabled autologin%b\n" "$GREEN" "$NC"
 fi
 
